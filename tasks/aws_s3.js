4b74d785c80cab698cadf54e5060b7bf4792d898
Joe Einertson <jeinerts@umm>
2014-05-03 01:14:46 -0500

    New option: only show files changed in differential upload

--- tasks/aws_s3.js
+++ tasks/aws_s3.js
@@ -35,7 +35,8 @@ module.exports = function (grunt) {
 			debug: false,
 			mock: false,
 			differential: false,
-			stream: false
+			stream: false,
+			diffChangesOnly: false
 		});
 
 		// To deprecate
@@ -728,7 +729,7 @@ module.exports = function (grunt) {
 							uploaded++;
 							grunt.log.writeln('- ' + file.src.cyan + ' -> ' + (object_url + file.dest).cyan);
 						}
-						else {
+						else if (!options.diffChangesOnly) {
 							grunt.log.writeln('- ' + file.src.yellow + ' === ' + (object_url + file.dest).yellow);
 						}
 					});
